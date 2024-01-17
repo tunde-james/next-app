4a7232062bef5b65294c3748b65c382c555861cf
Tunde James <tundejames04@gmail.com>
2024-01-17 19:25:24 +0100

    routing overview

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,34 +1,10 @@
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import UserTable from './UserTable';
 
 async function UsersPage() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-    next: { revalidate: 10 },
-  });
-  const users: User[] = await res.json();
-
   return (
     <>
       <h1>Users</h1>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   );
 }
