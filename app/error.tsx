da3ef1880391babf563922bb68890fae74266591
Tunde James <tundejames04@gmail.com>
2024-01-18 15:46:56 +0100

    handle unexpected error

---
+++ app/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+interface Props {
+  error: Error;
+  reset: () => void;
+}
+
+function ErrorPage({ error, reset }: Props) {
+  console.log('Error', error);
+
+  return (
+    <>
+      <div>An unexpected error has occurred.</div>
+      <button className="btn" onClick={() => reset()}>
+        Retry
+      </button>
+    </>
+  );
+}
+
+export default ErrorPage;
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function NotFoundPage() {
-  return <div>The requested page doesn't exist.</div>;
+  return <div>The requested page doesn&apos;t exist.</div>;
 }
 
 export default NotFoundPage;
