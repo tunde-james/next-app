3824bcf1e1deb82e79bcc2c4ad9be4b1bc4612b0
Tunde James <tundejames04@gmail.com>
2024-01-20 17:26:46 +0100

    Create route handlers for GET, POST, PUT, and DELETE

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+import schema from '../schema';
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+
+  const validation = schema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  if (params.id > 10) {
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  }
+
+  return NextResponse.json({ id: 1, name: body.name, price: body.price });
+}
