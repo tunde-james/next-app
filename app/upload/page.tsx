c7d3d3c5e010ba48ab6d0f55cbd306f4e17afb62
Tunde James <tundejames04@gmail.com>
2024-01-22 11:32:42 +0100

    Implement showing uploaded images and customizing the upload widget.

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,40 @@
 'use client';
 
-import { CldUploadWidget } from 'next-cloudinary';
+import { useState } from 'react';
+import { CldUploadWidget, CldImage } from 'next-cloudinary';
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 function UploadPage() {
+  const [publicId, setPublicId] = useState('');
+
   return (
-    <CldUploadWidget uploadPreset="hk4vmazn">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage src={publicId} alt="A painting" width={270} height={180} />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="hk4vmazn"
+        options={{
+          sources: ['local'],
+          multiple: false,
+          maxFiles: 5,
+        }}
+        onUpload={(result, widget) => {
+          if (result.event !== 'success') return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 }
 
