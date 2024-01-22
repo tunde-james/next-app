745e38402861870be50e1f1bca009772067678ae
Tunde James <tundejames04@gmail.com>
2024-01-22 09:52:32 +0100

    create the GET and POST api endpoint for products

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,19 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/prisma/client';
 import schema from './schema';
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    {
-      id: 1,
-      name: 'Milk',
-      price: 2.5,
-    },
-    {
-      id: 2,
-      name: 'Bread',
-      price: 3.5,
-    },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
@@ -24,8 +15,12 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  return NextResponse.json(
-    { id: 10, name: body.name, price: body.price },
-    { status: 201 }
-  );
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(newProduct, { status: 201 });
 }
