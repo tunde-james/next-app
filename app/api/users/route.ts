0f6ad557987e8033a9981912154f42818b8c330a
Tunde James <tundejames04@gmail.com>
2024-01-22 09:13:59 +0100

    Update the GET api to fetch many and unique user from the database with prisma

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/prisma/client';
 import schema from './schema';
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: 'Mosh' },
-    { id: 2, name: 'John' },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
