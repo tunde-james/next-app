4934e12d1eab00a68a9d7a1722684a22441ed8b6
Tunde James <tundejames04@gmail.com>
2024-01-22 09:29:21 +0100

    update the POST api endpoint to create a new user with prisma

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -14,5 +14,21 @@ export async function POST(request: NextRequest) {
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user) {
+    return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
